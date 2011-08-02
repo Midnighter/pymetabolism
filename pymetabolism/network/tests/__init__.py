4a3138a96039aea9243cf319297a0f59f1ebf30a
Moritz Emanuel Beber <m.beber@jacobs-university.de>
2011-08-02 23:38:16 +0200

    init files for tests, can be used for module wide setup

---
+++ pymetabolism/network/tests/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+================================
+Python Metabolic Modelling Tests
+================================
+
+:Authors:
+    Moritz Emanuel Beber
+:Date:
+    2011-04-12
+:Copyright:
+    Copyright(c) 2011 Jacobs University of Bremen. All rights reserved.
+:File:
+    __init__.py
+"""
+
